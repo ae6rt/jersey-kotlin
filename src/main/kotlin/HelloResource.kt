f4bfc4ef9b11c92da90147c0d6ddc1c01b31dbff
Mark S. Petrovic <mspetrovic@gmail.com>
2012-03-17 15:56:58 -0700

    Remove square braces around JAX-RS annotations

--- src/main/kotlin/HelloResource.kt
+++ src/main/kotlin/HelloResource.kt
@@ -1,14 +1,10 @@
-/**
- @author petrovic -- 3/4/12 4:46 PM
- */
-
 import javax.ws.rs.Path
 import javax.ws.rs.GET
 
-[Path("/hello")]
+Path("/hello")
 class HelloResource() {
 
-    [GET]
+    GET
     fun getHello() : String {
         return "hello"
     }
