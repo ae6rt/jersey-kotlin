c5d3899ebce91a1a1fefa3dfdda7e519af938b2e
Mark S. Petrovic <mspetrovic@gmail.com>
2012-03-18 05:53:03 -0700

    Add query param to resource handler

--- src/main/kotlin/HelloResource.kt
+++ src/main/kotlin/HelloResource.kt
@@ -1,11 +1,15 @@
-import javax.ws.rs.Path
+package org.petrovic.ws
+
+
 import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.QueryParam
 
 Path("/hello")
 class HelloResource() {
 
     GET
-    fun getHello() : String {
-        return "hello"
+    fun getHello([QueryParam("name")] qs : String) : String {
+        return "hello, " + qs
     }
 }
